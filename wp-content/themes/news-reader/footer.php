04035ff5bcb88a72299effede0a75c9a793a94bc
Cedric Takongmo <cedric.takongmo@janitza.de>
2017-11-17 13:30:19 +0100

    footer widget position changed

--- wp-content/themes/news-reader/footer.php
+++ wp-content/themes/news-reader/footer.php
@@ -8,21 +8,23 @@
  *
  * @package News_Reader
  */
-
 ?>
 
-	   
-<?php 
+
+<?php
 /**
-* Hook - newsreader_footer_container.
-*
-* @hooked newsreader_footer_container_before - 10
-* @hooked newsreader_footer_container - 11
-* @hooked newsreader_footer_container_copy_right - 12
-* @hooked newsreader_footer_container_after - 13
-*/
-do_action('newsreader_footer_container');?>
-<?php if( function_exists('slbd_display_widgets') ) { echo slbd_display_widgets(); } ?>
+ * Hook - newsreader_footer_container.
+ *
+ * @hooked newsreader_footer_container_before - 10
+ * @hooked newsreader_footer_container - 11
+ * @hooked newsreader_footer_container_copy_right - 12
+ * @hooked newsreader_footer_container_after - 13
+ */
+if (function_exists('slbd_display_widgets')) {
+    echo slbd_display_widgets();
+}
+do_action('newsreader_footer_container');
+?>
 <?php wp_footer(); ?>
 </body>
 </html>
